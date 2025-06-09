73ae17508473f9c2c31c1ddf359398da32ba5a0b
Byron Griggs <b.griggs-2022@hull.ac.uk>
2025-06-09 12:09:56 +0100

    Added code and explanation for dissertation

---
+++ 2025-9 Master's Dissertation/Interface/Client/ControlHijack.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+
+namespace AwariaInterface
+{
+    // Note: No effort has been made to ensure anything in this solution is thread safe
+    public class ControlHijack : MonoBehaviour
+    {
+        public static ControlHijack Instance { get; private set; }
+        // These control values are set by ControlHijack each frame
+        // and are given to Awaria when it requests them.
+        private Vector2 _moveVector = Vector2.zero;
+        // NOTE: These booleans are meant to represent the rising edge. They should be set to false whenever the getter functions grab them, and never set to false by anything else.
+        private bool _primaryPressed = false;
+        private bool _secondaryPressed = false;
+        private bool _cancelPressed = false;
+
+        // Used for communication with other side of interface
+        AwariaClient _client;
+
+        void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string localLow = Path.Combine(userProfile, "AppData", "LocalLow");
+            string path = Path.Combine(localLow, "vanripper", "Awaria", "testfile.txt");
+            File.WriteAllText(path, "Byron's .dll has been loaded");
+            Debug.Log("Byron's .dll has been loaded 2");
+
+            _client = new AwariaClient();
+            _client.initialize();
+
+            // Debugging
+            //StartCoroutine(testSequence());
+        }
+        void Update()
+        {
+            _client.getMostRecent(ref _moveVector, ref _primaryPressed, ref _secondaryPressed, ref _cancelPressed);
+        }
+        public Vector2 getMoveVector() { return _moveVector; }
+        public bool getPrimary() { 
+            if (_primaryPressed)
+            {
+                _primaryPressed = false;
+                return true;
+            }
+            return false;
+        }
+        public bool getSecondary() { 
+            if (_secondaryPressed)
+            {
+                _secondaryPressed = false;
+                return true;
+            }
+            return false;
+        }
+        public bool getCancel() { 
+            if (_cancelPressed)
+            {
+                _cancelPressed = false;
+                return true;
+            }
+            return false;
+        }
+
+        private IEnumerator testSequence()
+        {
+            Debug.Log("testSequence started");
+            yield return new WaitForSecondsRealtime(5f);
+            for (int i = 0; i < 3; i++)
+            {
+                _primaryPressed = true;
+                yield return new WaitForSecondsRealtime(0.5f);
+            }
+            while (true)
+            {
+                _moveVector.x = 0.5f;
+                _moveVector.y = 0;
+                yield return new WaitForSecondsRealtime(0.3f);
+                _moveVector.x = 0;
+                _moveVector.y = 0.5f;
+                yield return new WaitForSecondsRealtime(0.3f);
+                _moveVector.x = -0.5f;
+                _moveVector.y = 0;
+                yield return new WaitForSecondsRealtime(0.3f);
+                _moveVector.x = 0;
+                _moveVector.y = -0.5f;
+                yield return new WaitForSecondsRealtime(0.3f);
+            }
+        }
+        void OnDestroy()
+        {
+            _client?.shutdown();
+        }
+    }
+}
---
+++ 2025-9 Master's Dissertation/Interface/README.md
@@ -0,0 +1,4 @@
+# Game Interface
+As the game has already been thoroughly tested and part of the goal of the project is to change it as little as possible, I developed a server/client architecture to isolate the game from my code. The client runs as a .dll inside of Awaria.exe, and communicates via pipes with an external application.
+The external application contains the neural network and training algorithm, and is implemented in C++ rather than C#. This is just so that I can code in a familiar environment.
+The client is expected to send game state data (e.g. player position) to the server, while the server is expected to send inputs. As of 9th June, only the inputs have been implemented, and the communication is thus one way.
